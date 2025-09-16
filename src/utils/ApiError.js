c211ce31690bba8c575cfd498d970592d846ec02
Dhaval-y <ujjwalvaish241304@gmail.com>
2025-09-16 10:46:38 +0530

    Learned About API Errors

---
+++ src/utils/ApiError.js
@@ -0,0 +1,22 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        errors = [],
+        statck = ""
+    ){
+        super(message);
+        this.statusCode = statusCode;
+        this.message = message;
+        this.errors = errors;
+        this.success = false;
+        this.data = null;
+
+        if(statck){
+            this.stack = statck;
+        }   
+        else{
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
