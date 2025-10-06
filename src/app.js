dd925502f7e2679a4963b048c08b0f32608fc203
Dhaval-y <ujjwalvaish241304@gmail.com>
2025-10-06 10:46:26 +0530

    Created Middlewares, Routes

--- src/app.js
+++ src/app.js
@@ -8,9 +8,18 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
+
+
 app.use(express.json({limit: "50kb"}))
 app.use(express.urlencoded({extended: true, limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//route import
+import userRouter from "./routes/user.routes.js"
+
+// route declaration
+app.use("/users", userRouter) 
+
 
 export {app}
